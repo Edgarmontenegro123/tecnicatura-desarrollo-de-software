ff8a7dd7c85d6d4fa6042c00fc8263f7841d0de8
Edgar Montenegro <edgarmontenegro321@gmail.com>
2024-10-12 22:02:40 -0300

    🥡 Add extra practise file

---
+++ Cuatrimestre 01/Tecnicas de programación/Extra_practise_test_01.js
@@ -0,0 +1,71 @@
+// Extra practise
+
+/*
+    1) Imprimir una pirámide de números: Realizar un programa que permita al usuario ingresar un número n y que muestre por pantalla una pirámide de números.
+    Por ejemplo, si el usuario ingresa el número 5, el programa debería mostrar:
+    1
+    1 2
+    1 2 3
+    1 2 3 4
+    1 2 3 4 5
+ */
+
+/*
+    2) Número al revés: Escribir un algoritmo que solicite al usuario un número entero y que muestre por pantalla el mismo número pero invertido.
+    Por ejemplo, si el usuario ingresa 1234, el programa debería mostrar 4321. No utilices cadenas ni métodos nativos de inversión.
+*/
+
+/*
+    3) Calcular sumatoria de los primeros n números pares: Realizar un algoritmo que solicite al usuario un número n y muestre la sumatoria de
+    los primeros n números pares. Si el usuario ingresa 4, el resultado debería ser 2 + 4 + 6 + 8 = 20.
+*/
+
+/*
+    4) Imprimir una figura en forma de rombo: El usuario ingresa un número n y el programa debe mostrar una figura en forma de rombo formada por asteriscos.
+    Por ejemplo, si el usuario ingresa 5, el programa debe imprimir algo similar a esto:
+    *
+    * *
+    * * *
+    * *
+    *
+*/
+
+/*
+    5) Contar los números divisibles por 2, 3 y 5: Realizar un programa que solicite al usuario ingresar un número entero positivo y que cuente cuántos números
+    entre 1 y ese número son divisibles por 2, 3 o 5, mostrando los resultados por separado. Por ejemplo, si el número ingresado es 15, el programa debería
+    contar cuántos números son divisibles por 2, por 3 y por 5.
+*/
+
+/*
+    6) Determinar si una palabra es un anagrama de otra: Crear un programa que permita al usuario ingresar dos palabras y determine si son anagramas.
+    Un anagrama es cuando las letras de una palabra pueden reorganizarse para formar la otra. No utilices métodos nativos de manipulación de cadenas.
+    Debes comparar manualmente las letras.
+*/
+
+/*
+    7) Generar una secuencia alternada de números pares e impares: Solicitar al usuario un número n y generar una secuencia alternada de números pares e
+    impares hasta el número n. Por ejemplo, si el usuario ingresa 8, la secuencia debería ser 1, 2, 3, 4, 5, 6, 7, 8.
+*/
+
+/*
+    8) Sumatoria de múltiplos: Realizar un programa que solicite al usuario un número n y calcule la sumatoria de todos los múltiplos de 3 y 5 entre 1 y n.
+    Por ejemplo, si el número ingresado es 10, la sumatoria sería 3 + 5 + 6 + 9 = 23.
+*/
+
+/*
+    9) Convertir números a palabras (versión básica): Realizar un programa que solicite al usuario un número entre 1 y 10 y muestre por pantalla el nombre
+    de ese número. Por ejemplo, si el usuario ingresa 3, el programa debería mostrar "tres". Implementarlo utilizando una estructura switch.
+*/
+
+/*
+    10) Secuencia de Collatz: La conjetura de Collatz establece que, dados ciertos números enteros positivos:
+    Si el número es par, lo divides entre 2.
+    Si el número es impar, lo multiplicas por 3 y le sumas 1.
+    Repetir este proceso hasta llegar al número 1. Realiza un programa que solicite al usuario un número y muestre la secuencia de Collatz
+    generada por dicho número.
+*/
+
+
+
+
+
