1f434fb0a35d411766d4b60f3d49dc46886037d5
Edgar Montenegro <edgarmontenegro321@gmail.com>
2024-10-28 19:13:27 -0300

    🗺️ Add TP6 to practice arrays

---
+++ Cuatrimestre 01/Tecnicas de programación/TP6.js
@@ -0,0 +1,30 @@
+// TP 6 Técnicas de programación
+
+const prompt = require('prompt-sync')()
+
+
+/*1). Dado un array compuesto por la cantidad de números aleatorios que determine el usuario, realizar un programa que muestre por pantalla el promedio
+      de esos números, y cuál es el número que se encuentra en la mitad del array.
+*/
+
+/*2). Dado un array ordenado, buscar los números ingresados por usuario, indicando la posición del vector en que se encuentra o bien, mostrando por pantalla
+      el mensaje de que el número no forma parte del vector.
+*/
+
+/*3). Dado un array compuesto por la cantidad de números aleatorios que determine el usuario, realizar un programa que muestre por pantalla el mismo array,
+      pero con sus componentes invertidos, es decir, el último elemento en el primer lugar y lo mismo con el resto de los números.
+      Por ejemplo el array [4, 5, 1, 6, 8, 9, 0, 2, 7] invertido es: [7, 2, 0, 9, 8, 6, 1, 5, 4].
+*/
+
+/*4). Dados dos arrays de 10 posiciones que almacenan números enteros.
+      Realice un programa que determine si esos arrays son iguales (los arrays son iguales si los números en las mismas posiciones son iguales.
+*/
+
+/*5). Dado un array compuesto por la cantidad de números aleatorios que determine el usuario, realizar un programa que muestre por pantalla los
+      números repetidos y la cantidad de veces que se repitieron.
+*/
+
+/*6). Dado un array compuesto por la cantidad de números aleatorios que determine el usuario, realizar un programa que ordene el array de menor a mayor.
+
+      6.1 Agregar un menu para que el usuario pueda determinar de qué manera ordenar el array.
+*/
